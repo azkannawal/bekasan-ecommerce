7d5b1f6bf80aee5d5abec9efa4b3849b154c7dec
azkannawal <azkannawalaf@gmail.com>
2024-03-07 22:05:39 +0700

    feat: landingpage-

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home.tsx";
+import NotFound from "./pages/notfound.tsx";
+import AdressInput from "./components/fragments/AdressInput.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/gmaps",
+    element: <AdressInput />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/home.tsx
@@ -0,0 +1,66 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+const Home = () => {
+  const feature = [
+    "Fitur Jual Beli Barang Bekas",
+    "Fitur Jarak Terdekat",
+    "Fitur Pesan ke Penjual",
+  ];
+  const description = [
+    "Memudahkan Anda dalam menjual atau membeli barang bekas",
+    "Memudahkan Anda dalam menemukan barang bekas terdekat",
+    "Memudahkan Anda dalam menentukan waktu bertemu untuk COD",
+  ];
+
+  return (
+    <main className="flex flex-col min-h-screen justify-center items-center">
+      <Carousel className="w-full max-w-3xl">
+        <h1 className="uppercase text-center font-bold text-3xl mb-7">
+          Selamat Datang
+        </h1>
+        <CarouselContent>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="flex flex-col items-center justify-center gap-3">
+                  <img
+                    className="h-96"
+                    src={`./landingpage0${index + 1}.png`}
+                    alt="img"
+                  />
+                  <p className="uppercase font-bold text-xl mt-3">
+                    {feature[index]}
+                  </p>
+                  <p className="text-lg">{description[index]}</p>
+                </CardContent>
+              </Card>
+              <div className="flex justify-center items-center mt-7">
+                {[0, 1, 2].map((item) => (
+                  <span
+                    key={item}
+                    className={`w-4 h-4 mx-1.5 rounded-full ${
+                      index === item
+                        ? "bg-black animate-pulse duration-800"
+                        : "bg-gray-300"
+                    }`}
+                  />
+                ))}
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </main>
+  );
+};
+
+export default Home;
