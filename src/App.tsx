19a66a890aa211c937323a32d4ba91c64eeeba0c
azkannawal <azkannawalaf@gmail.com>
2024-03-07 11:47:32 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <Button className="rounded-full bg-red-900">Mantap</Button>
+    </div>
+  );
+}
+
+export default App;
