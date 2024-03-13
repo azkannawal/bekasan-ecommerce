503239327a5068c2d2263f14b001448a78a4abf8
azkannawal <azkannawalaf@gmail.com>
2024-03-13 21:55:44 +0700

    fix: filter state

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -16,31 +16,29 @@ import { IoIosArrowDown } from "react-icons/io";
 type Checked = DropdownMenuCheckboxItemProps["checked"];
 
 const Search = () => {
-  const categories = [
-    { name: "Sepatu", state: useState<Checked>(false) },
-    { name: "Tas", state: useState<Checked>(false) },
-    { name: "Lemari", state: useState<Checked>(false) },
-  ];
-  const prices = [
-    { name: "Rp 0 - Rp 249.999", state: useState<Checked>(false) },
-    { name: "Rp 250.000 - Rp 499.999", state: useState<Checked>(false) },
-    { name: "Rp 500.000 - Rp 999.999", state: useState<Checked>(false) },
-    { name: "> Rp 1.000.000", state: useState<Checked>(false) },
-  ];
-  const distances = [
-    { name: "0 - 1 KM", state: useState<Checked>(false) },
-    { name: "1,1 KM - 2 KM", state: useState<Checked>(false) },
-    { name: "2,1 KM - 4 KM", state: useState<Checked>(false) },
-    { name: "> 4 KM", state: useState<Checked>(false) },
-  ];
-  const [dropdowns, setDropdowns] = useState([0, 0, 0]);
+  const [dropdown1, setDropdown1] = useState(0);
+  const [dropdown2, setDropdown2] = useState(0);
+  const [dropdown3, setDropdown3] = useState(0);
+  const [category1, setCategory1] = useState<Checked>(false);
+  const [category2, setCategory2] = useState<Checked>(false);
+  const [category3, setCategory3] = useState<Checked>(false);
+  const [price1, setPrice1] = useState<Checked>(false);
+  const [price2, setPrice2] = useState<Checked>(false);
+  const [price3, setPrice3] = useState<Checked>(false);
+  const [price4, setPrice4] = useState<Checked>(false);
+  const [distance1, setDistance1] = useState<Checked>(false);
+  const [distance2, setDistance2] = useState<Checked>(false);
+  const [distance3, setDistance3] = useState<Checked>(false);
+  const [distance4, setDistance4] = useState<Checked>(false);
 
-  const toggleDropdown = (index: number): void => {
-    setDropdowns((prevDropdowns) => {
-      const newDropdowns = [...prevDropdowns];
-      newDropdowns[index] = newDropdowns[index] === 0 ? 180 : 0;
-      return newDropdowns;
-    });
+  const toggleDropdown = (field: any): void => {
+    if (field === 1) {
+      setDropdown1((prevState) => (prevState === 0 ? 180 : 0));
+    } else if (field === 2) {
+      setDropdown2((prevState) => (prevState === 0 ? 180 : 0));
+    } else {
+      setDropdown3((prevState) => (prevState === 0 ? 180 : 0));
+    }
   };
 
   return (
@@ -51,104 +49,137 @@ const Search = () => {
           <div className="flex flex-col fixed gap-4 pt-24 px-6 w-1/5">
             <h1 className="text-2xl font-bold text-white px-2">Filter</h1>
 
-            {categories.map((item, index) => (
-              <DropdownMenu
-                key={index}
-                onOpenChange={() => toggleDropdown(index)}
-              >
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className="justify-between px-2 text-white"
-                  >
-                    {item.name}
-                    <IoIosArrowDown
-                      className="mr-2 h-4 w-4"
-                      style={{
-                        transition: "transform 0.5s",
-                        transform: `rotate(${dropdowns[index]}deg)`,
-                      }}
-                    />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-60">
-                  <DropdownMenuLabel>{item.name}</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem
-                    checked={item.state[0]}
-                    onCheckedChange={item.state[1]}
-                  >
-                    {item.name}
-                  </DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            ))}
+            <DropdownMenu onOpenChange={() => toggleDropdown(1)}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="justify-between px-2 text-white"
+                >
+                  Kategori
+                  <IoIosArrowDown
+                    className="mr-2 h-4 w-4"
+                    style={{
+                      transition: "transform 0.5s",
+                      transform: `rotate(${dropdown1}deg)`, // Menggunakan nilai state untuk rotasi ikon
+                    }}
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-60">
+                <DropdownMenuLabel>Kategori</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuCheckboxItem
+                  checked={category1}
+                  onCheckedChange={setCategory1}
+                >
+                  Sepatu
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={category2}
+                  onCheckedChange={setCategory2}
+                >
+                  Tas
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={category3}
+                  onCheckedChange={setCategory3}
+                >
+                  Lemari
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
 
-            {prices.map((item, index) => (
-              <DropdownMenu
-                key={index}
-                onOpenChange={() => toggleDropdown(index)}
-              >
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className="justify-between px-2 text-white"
-                  >
-                    {item.name}
-                    <IoIosArrowDown
-                      className="mr-2 h-4 w-4"
-                      style={{
-                        transition: "transform 0.5s",
-                        transform: `rotate(${dropdowns[index]}deg)`,
-                      }}
-                    />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-60">
-                  <DropdownMenuLabel>{item.name}</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem
-                    checked={item.state[0]}
-                    onCheckedChange={item.state[1]}
-                  >
-                    {item.name}
-                  </DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            ))}
+            <DropdownMenu onOpenChange={() => toggleDropdown(2)}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="justify-between px-2 text-white"
+                >
+                  Harga
+                  <IoIosArrowDown
+                    className="mr-2 h-4 w-4"
+                    style={{
+                      transition: "transform 0.5s",
+                      transform: `rotate(${dropdown2}deg)`,
+                    }}
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-60">
+                <DropdownMenuLabel>Harga</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuCheckboxItem
+                  checked={price1}
+                  onCheckedChange={setPrice1}
+                >
+                  Rp 0 - Rp 249.999
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={price2}
+                  onCheckedChange={setPrice2}
+                >
+                  Rp 250.000 - Rp 499.999
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={price3}
+                  onCheckedChange={setPrice3}
+                >
+                  Rp 500.000 - Rp 999.999
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={price4}
+                  onCheckedChange={setPrice4}
+                >
+                  {`> Rp 1.000.000`}
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
 
-            {distances.map((item, index) => (
-              <DropdownMenu
-                key={index}
-                onOpenChange={() => toggleDropdown(index)}
-              >
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className="justify-between px-2 text-white"
-                  >
-                    {item.name}
-                    <IoIosArrowDown
-                      className="mr-2 h-4 w-4"
-                      style={{
-                        transition: "transform 0.5s",
-                        transform: `rotate(${dropdowns[index]}deg)`,
-                      }}
-                    />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-60">
-                  <DropdownMenuLabel>{item.name}</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem
-                    checked={item.state[0]}
-                    onCheckedChange={item.state[1]}
-                  >
-                    {item.name}
-                  </DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            ))}
+            <DropdownMenu onOpenChange={() => toggleDropdown(3)}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="justify-between px-2 text-white"
+                >
+                  Jarak
+                  <IoIosArrowDown
+                    className="mr-2 h-4 w-4"
+                    style={{
+                      transition: "transform 0.5s",
+                      transform: `rotate(${dropdown3}deg)`, // Menggunakan nilai state untuk rotasi ikon
+                    }}
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-60">
+                <DropdownMenuLabel>Jarak</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuCheckboxItem
+                  checked={distance1}
+                  onCheckedChange={setDistance1}
+                >
+                  0 - 1 KM
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={distance2}
+                    onCheckedChange={setDistance2}
+                >
+                  1,1 KM - 2 KM
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={distance3}
+                    onCheckedChange={setDistance3}
+                >
+                  2,1 KM - 4 KM
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={distance4}
+                    onCheckedChange={setDistance4}
+                >
+                  {`> 4 KM`}
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
         <CardProduct style="w-4/5 px-6" />
