04cf6b3a1c98bf1813d205160276963e8e944744
azkannawal <azkannawalaf@gmail.com>
2024-03-15 14:21:59 +0700

    feat: notfound page

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center min-h-screen bg-[#135699]">
+      <p className="font-semibold text-lg text-white">
+        Halaman tidak ditemukan
+      </p>
+      <Link to="/">
+        <Button>Kembali ke Home</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
